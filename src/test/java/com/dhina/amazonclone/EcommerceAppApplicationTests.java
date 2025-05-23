9234ef9b7c04cba629000b2cd549e1f4237151b6
dhinathayaalan <rishabdhina2804@gmail.com>
2025-05-23 20:52:40 +0530

    Project has been Initialized

---
+++ src/test/java/com/dhina/amazonclone/EcommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhina.amazonclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
